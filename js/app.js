a8272146847382b9117a6a79c2a96f9d1e3b02b2
{Zin} <{maohvhn@gmail.com}>
2022-11-24 09:27:07 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,43 +1,47 @@
-var search = document.querySelector(".search");
-var city = document.querySelector(".city");
-var country = document.querySelector(".country");
-var value = document.querySelector(".value");
-var shortDesc = document.querySelector(".short-desc");
-var visibility = document.querySelector(".visibility span");
-var wind = document.querySelector(".wind span");
-var sun = document.querySelector(".sun span");
-var time = document.querySelector(".time");
-var content = document.querySelector(".content");
+var search = $(".search");
+var city = $(".city");
+var country = $(".country");
+var shortDesc = $(".short-desc");
+var visibility = $(".visibility span");
+var wind = $(".wind span");
+var sun = $(".sun span");
+var time = $(".time");
+var value = $(".value");
+var content = $(".content");
 
-async function changeWeatherUI(capitalSearch) {
-  //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=eddd192e840df6217ceddcb529b78755`;
+$(document).ready(function () {
+  function changeWeatherUI(capitalSearch) {
+    var urlApi = `http://api.weatherapi.com/v1/current.json?key=37b7bd39d46b412598090030222211&q=${capitalSearch}&aqi=no`;
 
-  let apiUrlv2 = `http://api.weatherapi.com/v1/current.json?key=37b7bd39d46b412598090030222211&q=${capitalSearch}&aqi=no`;
-
-  let data = await fetch(apiUrlv2).then((res) => res.json());
-  if (data.location != null) {
-    // content.classList.remove("hide");
-    city.innerText = data.location.name;
-    country.innerText = data.location.country;
-    visibility.innerText = data.current + "%";
-    visibility.innerText = data.current.cloud + "%";
-    wind.innerHTML = data.current.wind_degree + "m/s";
-    sun.innerHTML = data.current.humidity + "%";
-    value.innerHTML = Math.round(data.current.temp_c) + `<sup>o</sup>C`;
-    shortDesc.innerText = data.current.condition
-      ? data.current.condition.text
-      : "";
-    time.innerText = data.location.localtime;
-    //   console.log("API:", data);
-  } else {
-    content.classList.add("hide");
+    $.ajax({
+      type: "get",
+      url: urlApi,
+      dataType: "json",
+      success: function (rs, status) {
+        if (rs.location != null) {
+          content.removeClass("hide");
+          city.html(rs.location.name);
+          country.html(rs.location.country);
+          time.html(rs.location.localtime);
+          shortDesc.html(rs.current.condition.text);
+          value.html(Math.round(rs.current.temp_c) + `<sup>o</sup>C`);
+          sun.html(rs.current.humidity + "%");
+          wind.html(rs.current.wind_degree + "m/s");
+          visibility.html(rs.current.cloud + "%");
+        }
+      },
+      error: function () {
+        content.addClass("hide");
+      },
+    });
+    search.keypress(function (e) {
+      if (e.code == "Enter") {
+        let capitalSearch = search.val().trim();
+        console.log(capitalSearch);
+        changeWeatherUI(capitalSearch);
+      }
+    });
   }
-}
-search.addEventListener("keypress", function (e) {
-  if (e.code === "Enter") {
-    let capitalSearch = search.value.trim();
-    changeWeatherUI(capitalSearch);
-  }
+
+  changeWeatherUI("hanoi");
 });
-
-changeWeatherUI("hanoi");
